f6d2e2d53d6be7b634be4e40c9875afca7e9d3f6
Stefano <stefano.dipaola@mindedsecurity.com>
2018-08-23 10:24:21 +0200

    updated url example to something useful

--- puppeteer_test.js
+++ puppeteer_test.js
@@ -13,9 +13,10 @@ if (!bcconfig.bcdetect_home_dir)
 
     const browser = await bc_puppeteer.launch(bcconfig);
     const page = await browser.newPage();
-// Add your Scriptimg here:
+// Add your Scripting here:
 
     await page.goto('http://www.domxss.com/domxss/01_Basics/00_simple_noHead.html?13133862');
+    //await page.goto('http://127.0.0.1:5000/dom?');
     await page.evaluate('_rw_.fuzzPage();');
     //await browser.close();
 })();
